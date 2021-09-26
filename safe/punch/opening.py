2b6627ce10a289833f840329ed03d78232eaa0c6
ebrahimraeyat <ebe79442114@yahoo.com>
2021-09-26 16:16:31 +0330

    adding opening object

---
+++ safe/punch/opening.py
@@ -0,0 +1,98 @@
+import math
+from pathlib import Path
+import Part
+import FreeCAD
+from etabs_api.frame_obj import FrameObj
+
+def make_opening(points, height):
+    obj = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", "Opening")
+    Opening(obj)
+    obj.points = points
+    obj.height = height
+    if FreeCAD.GuiUp:
+        _ViewProviderOpening(obj.ViewObject)
+    FreeCAD.ActiveDocument.recompute()
+    return obj
+
+
+class Opening:
+    def __init__(self, obj):
+        obj.Proxy = self
+        self.Type = "opening"
+        self.set_properties(obj)
+
+    def set_properties(self, obj):
+        if not hasattr(obj, "points"):
+            obj.addProperty(
+            "App::PropertyVectorList",
+            "points",
+            "Geometry",
+            )
+        if not hasattr(obj, "plane"):
+            obj.addProperty(
+                "Part::PropertyPartShape",
+                "plane",
+                "openning",
+                )
+        # if not hasattr(obj, "solid"):
+        #     obj.addProperty(
+        #         "Part::PropertyPartShape",
+        #         "solid",
+        #         "openning",
+        #         )
+        # if not hasattr(obj, "foundation"):
+        #     obj.addProperty(
+        #         "Part::PropertyLink",
+        #         "foundation",
+        #         "openning",
+        #         )
+        if not hasattr(obj, "height"):
+            obj.addProperty(
+            "App::PropertyLength",
+            "height",
+            "slab",
+            )
+
+    def onChanged(self, obj, prop):
+        return
+
+    def execute(self, obj):
+        points = obj.points + [obj.points[0]]
+        obj.plane = Part.Face(Part.makePolygon(points))
+        obj.Shape = obj.plane.extrude(FreeCAD.Vector(0, 0, -obj.height.Value))
+
+
+class _ViewProviderOpening:
+    def __init__(self, vobj):
+        vobj.Proxy = self
+        vobj.Transparency = 95
+        vobj.DisplayMode = "Shaded"
+
+    def attach(self, vobj):
+        self.ViewObject = vobj
+        self.Object = vobj.Object
+
+    def getIcon(self):
+        return str(Path(__file__).parent / "Resources" / "icons" / "opening.svg")
+
+    def __getstate__(self):
+        return None
+
+    def __setstate__(self, state):
+        return None
+
+
+if __name__ == "__main__":
+    x1 = 10
+    x2 = 25
+    y1 = 7
+    y2 = 17
+    p1=FreeCAD.Vector(x1, y1, 0)
+    p2=FreeCAD.Vector(x2, y1, 0)
+    p3=FreeCAD.Vector(x2, y2, 0)
+    p4=FreeCAD.Vector(x1, y2, 0)
+    points = [p1, p2, p3, p4]
+    make_opening(
+            points=points,
+            height = 3,
+               )
