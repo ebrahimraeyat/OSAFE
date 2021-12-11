7555d1760a1e632eab2d3eb2222a51bcdac59e6e
ebrahimraeyat <ebe79442114@yahoo.com>
2021-12-11 09:17:05 +0330

    adding column to the model

--- safe/punch/etabs_punch.py
+++ safe/punch/etabs_punch.py
@@ -2,18 +2,20 @@ try:
     # from safe.punch.axis import create_grids
     # from safe.punch.punch_funcs import remove_obj
     from safe.punch import beam
-    from safe.punch import etabs_foundation
+    # from safe.punch import etabs_foundation
     # from safe.punch import strip
-    # from safe.punch.punch import make_punch
+    # from safe.punch.column import make_column
 except:
     # from axis import create_grids
     # from punch_funcs import remove_obj
-    import etabs_foundation
+    # import etabs_foundation
     import beam
     # from punch import make_punch
 import Draft
 import FreeCAD
 import FreeCADGui as Gui
+import Arch
+import math
 from typing import Union
 
 
@@ -44,6 +46,130 @@ class EtabsPunch(object):
             v2 = FreeCAD.Vector(xj, yj, self.top_of_foundation)
             slabs[slab_name] = beam.make_beam(v1, v2)
         return slabs
+
+    # def create_foundation(self,
+    #     ):
+    #     self.create_slabs_plane()
+    #     Load_cases = self.etabs.load_cases.get_loadcase_withtype(1)
+    #     self.foundation = etabs_foundation.make_foundation(
+    #         self.cover, self.fc, self.height, self.foundation_type, Load_cases, self.top_of_foundation)
+
+    def create_columns(self):
+        joint_design_reactions = self.etabs.database.get_joint_design_reactions()
+        basepoints_coord_and_dims = self.etabs.database.get_basepoints_coord_and_dims(
+                joint_design_reactions
+            )
+        def make_column(
+            bx : float,
+            by : float,
+            center : FreeCAD.Vector,
+            angle : float,
+            combos_load : dict,
+            ):
+            col = Arch.makeStructure(length=bx,width=by,height=4000)
+            col.Placement.Base = center
+            col.Placement.Rotation.Angle = math.radians(angle)
+            if not hasattr(col, "combos_load"):
+                col.addProperty(
+                    "App::PropertyMap",
+                    "combos_load",
+                    "Structure",
+                    ).combos_load = combos_load
+            col.setEditorMode('combos_load', 2)
+            return col
+
+        # for f in self.foundation.Shape.Faces:
+        #     if f.BoundBox.ZLength == 0 and f.BoundBox.ZMax == 0:
+        #         foundation_plane = f
+        #         break
+        # self.foundation.plane = foundation_plane
+        columns = FreeCAD.ActiveDocument.addObject("App::DocumentObjectGroup","Columns")
+        for _, row in basepoints_coord_and_dims.iterrows():
+            name = row['UniqueName']
+            bx = float(row['t3'])
+            by = float(row['t2'])
+            if (not bx > 0) or (not by > 0):
+                continue
+            angle = float(row['AxisAngle'])
+            x = row['x']
+            y = row['y']
+            # z = row['z']
+            d = {}
+            df = joint_design_reactions[joint_design_reactions['UniqueName'] == name]
+            for _, row2 in df.iterrows():
+                combo = row2['OutputCase']
+                F = row2['FZ']
+                mx = row2['MX']
+                my = row2['MY']
+                d[combo] = f"{F}, {mx}, {my}"
+            center_of_load = FreeCAD.Vector(x, y, self.top_of_foundation)
+            col = make_column(
+                bx,
+                by,
+                center_of_load,
+                angle,
+                d,
+                )
+            
+            columns.addObject(col)
+        return columns
+    
+    
+    def create_punches(self):
+        joint_design_reactions = self.etabs.database.get_joint_design_reactions()
+        basepoints_coord_and_dims = self.etabs.database.get_basepoints_coord_and_dims(
+                joint_design_reactions
+            )
+        # for f in self.foundation.Shape.Faces:
+        #     if f.BoundBox.ZLength == 0 and f.BoundBox.ZMax == 0:
+        #         foundation_plane = f
+        #         break
+        # self.foundation.plane = foundation_plane
+        punches = []
+        for _, row in basepoints_coord_and_dims.iterrows():
+            name = row['UniqueName']
+            bx = float(row['t3'])
+            by = float(row['t2'])
+            if (not bx > 0) or (not by > 0):
+                continue
+            angle = float(row['AxisAngle'])
+            x = row['x']
+            y = row['y']
+            # z = row['z']
+            d = {}
+            df = joint_design_reactions[joint_design_reactions['UniqueName'] == name]
+            for _, row2 in df.iterrows():
+                combo = row2['OutputCase']
+                F = row2['FZ']
+                mx = row2['MX']
+                my = row2['MY']
+                d[combo] = f"{F}, {mx}, {my}"
+            center_of_load = FreeCAD.Vector(x, y, self.top_of_foundation)
+            p = make_punch(
+                # foundation_plane,
+                self.foundation,
+                bx,
+                by,
+                center_of_load,
+                d,
+                angle=angle,
+                )
+            l = p.Location
+            pl = FreeCAD.Vector(0, 0, self.top_of_foundation + 4100)
+            t = '0.0'
+            version = FreeCAD.Version()[1]
+            if int(version) < 19:
+                text = Draft.makeText([t, l], point=pl)
+            else:
+                text = Draft.make_text([t, l], placement=pl)
+            p.Ratio = t
+            if FreeCAD.GuiUp:
+                text.ViewObject.FontSize = 200
+            print(text)
+            p.text = text
+            p.id = name
+            punches.append(p)
+        return punches
     
     
     def grid_lines(self):
@@ -63,6 +189,7 @@ class EtabsPunch(object):
         name = self.etabs.get_file_name_without_suffix()
         FreeCAD.newDocument(name)
         self.create_slabs_plane()
+        self.create_columns()
         # self.create_foundation()
         # self.create_punches()
         FreeCAD.ActiveDocument.recompute()
