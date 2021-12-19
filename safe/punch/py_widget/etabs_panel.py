014fe597705fc06ac01067f85f59e89475eb34b0
ebrahimraeyat <ebe79442114@yahoo.com>
2021-12-20 00:37:37 +0330

    read steel columns to import to freecad

--- safe/punch/py_widget/etabs_panel.py
+++ safe/punch/py_widget/etabs_panel.py
@@ -64,7 +64,9 @@ class EtabsTaskPanel:
         selected_beams = self.form.selected_beams.isChecked()
         exclude_selected_beams = self.form.exclude_selected_beams.isChecked()
         beams_names = None
-        beams, _  = self.etabs.frame_obj.get_beams_columns(story=story)
+        conc_beams, _  = self.etabs.frame_obj.get_beams_columns(story=story)
+        steel_beams, _  = self.etabs.frame_obj.get_beams_columns(story=story, type_=1)
+        beams = steel_beams + conc_beams
         if (selected_beams or exclude_selected_beams):
             names = self.etabs.select_obj.get_selected_obj_type(2)
             names = [name for name in names if self.etabs.frame_obj.is_beam(name)]
