eec9820678dab27bd6861a07f0b15f7ea6b7a9ea
ebrahimraeyat <ebe79442114@yahoo.com>
2021-12-25 15:48:32 +0330

    minor change

--- InitGui.py
+++ InitGui.py
@@ -1,29 +1,19 @@
-import FreeCAD
-import os
 
 
 class CivilWorkbench(Workbench):
 
     def __init__(self):
-        rel_path = "Mod/Civil/images/civil-engineering.png"
-        pref_visual_ui_rel_path = "Mod/Civil/ui/preferences-punch_visual.ui"
-        self.pref_visual_ui_abs_path = FreeCAD.ConfigGet("AppHomePath") + pref_visual_ui_rel_path
-        import os
-        if not os.path.exists(self.pref_visual_ui_abs_path):
-            self.pref_visual_ui_abs_path = FreeCAD.ConfigGet("UserAppData") + pref_visual_ui_rel_path
 
-        icon_path = FreeCAD.ConfigGet("AppHomePath") + rel_path
-        if not os.path.exists(icon_path):
-            icon_path = FreeCAD.ConfigGet("UserAppData") + rel_path
-
-        self.__class__.Icon = icon_path
+        from pathlib import Path
+        import civilwelcome
+        self.__class__.Icon = str(Path(civilwelcome.__file__).parent.absolute() / 'images' / 'civil-engineering.png')
         self.__class__.MenuText = "Civil"
         self.__class__.ToolTip = "Civil Workbench"
 
     def Initialize(self):
-        from PySide2 import QtCore, QtGui
-        import civilGui
         from pathlib import Path
+        from PySide2 import QtCore
+        import civilGui
 
         command_list = civilGui.command_list
         export_list = civilGui.export_list
@@ -41,7 +31,8 @@ class CivilWorkbench(Workbench):
         self.appendMenu(str(QtCore.QT_TRANSLATE_NOOP("Civil", "Draw")), draw_list)
         self.appendMenu(str(QtCore.QT_TRANSLATE_NOOP("Civil", "Assign")), assign_list)
 
-        Gui.addPreferencePage(self.pref_visual_ui_abs_path, "punch")
+        pref_visual_ui_abs_path = str(Path(civilGui.__file__).parent.absolute() / 'ui' / 'preferences-punch_visual.ui')
+        Gui.addPreferencePage(pref_visual_ui_abs_path, "punch")
         Gui.addIconPath(
             str(
                 Path(civilGui.__file__).parent.absolute()
@@ -52,7 +43,7 @@ class CivilWorkbench(Workbench):
     def Activated(self):
         if FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Civil").GetBool("FirstTime", True):
             from DraftGui import todo
-            todo.delay(FreeCADGui.runCommand, "Civil_welcome")
+            todo.delay(Gui.runCommand, "Civil_welcome")
 
 
 Gui.addWorkbench(CivilWorkbench())
