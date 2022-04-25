e95820d6e2334b325de24b91a19033591a5b363f
ebrahimraeyat <ebe79442114@yahoo.com>
2022-04-26 01:12:05 +0430

    adding plan property to rectangular slab

--- safe/punch/rectangular_slab.py
+++ safe/punch/rectangular_slab.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Union
 from PySide2 import QtCore
-import FreeCAD
+import FreeCAD, Draft
 import ArchComponent
 
 try:
@@ -36,12 +36,23 @@ def make_rectangular_slab(
         obj.left_width = width - right_width
     elif align == 'Center':
         obj.left_width = obj.right_width = obj.width / 2
+    plan, _, _ = punch_funcs.get_left_right_offset_wire_and_shape(obj.Base.Shape, obj.left_width, obj.right_width)
+    points = punch_funcs.get_sort_points(plan.Edges)
+    plan = Draft.make_wire(points, closed=True)
+    # FreeCAD.ActiveDocument.recompute()
+    obj.plan = plan
     obj.align = align
     obj.height = height
     obj.ks = soil_modulus
     obj.fc = fc
     if FreeCAD.GuiUp:
         ViewProviderRectangularSlab(obj.ViewObject)
+        if obj.Base:
+            obj.Base.ViewObject.LineWidth = 3.00
+        if obj.plan:
+            obj.plan.ViewObject.LineWidth = 3.00
+        obj.ViewObject.Transparency = 20
+        obj.ViewObject.DisplayMode = "Flat Lines"
     FreeCAD.ActiveDocument.recompute()
     return obj
 
@@ -50,28 +61,34 @@ class RectangularSlab(ArchComponent.Component):
     def __init__(self, obj):
         super().__init__(obj)
         obj.IfcType = "Footing"
-        self.Type = "RectangularSlab"
         self.set_properties(obj)
-        obj.Proxy = self
 
     def set_properties(self, obj):
+        self.Type = "RectangularSlab"
+        obj.Proxy = self
         if not hasattr(obj, "layer"):
             obj.addProperty(
                 "App::PropertyEnumeration",
                 "layer",
                 "Strip",
+                "",
+                8,
                 ).layer = ['A', 'B', 'other']
         if not hasattr(obj, "design_type"):
             obj.addProperty(
                 "App::PropertyEnumeration",
                 "design_type",
                 "Strip",
+                "",
+                8,
                 ).design_type = ['column']
         if not hasattr(obj, "ks"):
             obj.addProperty(
                 "App::PropertyFloat",
                 "ks",
                 "Soil",
+                "",
+                8,
                 )
         if not hasattr(obj, "left_width"):
             obj.addProperty(
@@ -111,26 +128,30 @@ class RectangularSlab(ArchComponent.Component):
                 )
         if not hasattr(obj, "plan"):
             obj.addProperty(
-                "Part::PropertyPartShape",
+                "App::PropertyLink",
                 "plan",
                 "Geometry",
+                "",
+                16,
                 )
         if not hasattr(obj, "fc"):
             obj.addProperty(
             "App::PropertyPressure",
             "fc",
             "Concrete",
+            "",
+            8,
             )
+        if not hasattr(obj, "Auto_Plan"):
+            obj.addProperty(
+            "App::PropertyBool",
+            "Auto_Plan",
+            "Geometry",
+            ).Auto_Plan = False
 
     def execute(self, obj):
         if obj.width.Value == 0:
             return
-        # if obj.layer == 'A':
-        #     obj.ViewObject.ShapeColor = (1.00,0.00,0.20)
-        # elif obj.layer == 'B':
-        #     obj.ViewObject.ShapeColor = (0.20,0.00,1.00)
-        # elif obj.layer == 'other':
-        #     obj.ViewObject.ShapeColor = (0.20,1.00,0.00)
         if obj.align == 'Left':
             sl = obj.left_width.Value
             sr = obj.width.Value - sl
@@ -143,23 +164,27 @@ class RectangularSlab(ArchComponent.Component):
             sr = sl = obj.width.Value / 2
             obj.right_width = sr
             obj.left_width = sl
-        obj.plan, _, _ = punch_funcs.get_left_right_offset_wire_and_shape(obj.Base.Shape, sl, sr)
-        shape = obj.plan.extrude(FreeCAD.Vector(0, 0, -obj.height.Value))
-        obj.Shape = shape
-
+        if obj.Auto_Plan and obj.Base:
+            plan, _, _ = punch_funcs.get_left_right_offset_wire_and_shape(obj.Base.Shape, sl, sr)
+            points = punch_funcs.get_sort_points(plan.Edges, get_last=True)
+            obj.plan.Points = points
+        if obj.plan:
+            obj.Shape = obj.plan.Shape.extrude(FreeCAD.Vector(0, 0, -obj.height.Value))
 
 class ViewProviderRectangularSlab:
     def __init__(self, vobj):
         vobj.Proxy = self
-        vobj.Transparency = 20
-        vobj.DisplayMode = "Flat Lines"
 
     def attach(self, vobj):
         self.ViewObject = vobj
         self.Object = vobj.Object
 
     def claimChildren(self):
-        children = [FreeCAD.ActiveDocument.getObject(self.Object.Base.Name)]
+        children = []
+        if self.Object.Base:
+            children.append(FreeCAD.ActiveDocument.getObject(self.Object.Base.Name))
+        if self.Object.plan:
+            children.append(FreeCAD.ActiveDocument.getObject(self.Object.plan.Name))
         return children
 
     def getIcon(self):
@@ -183,14 +208,13 @@ class ViewProviderRectangularSlab:
         
 
 if __name__ == "__main__":
-    from safe.punch.beam import make_beam
     p1 = FreeCAD.Vector(0, 0, 0)
     p2 = FreeCAD.Vector(1000, 0, 0)
     p3 = FreeCAD.Vector(3000, 2000, 0)
     import Draft
     wire = Draft.make_wire([p1, p2, p3])
+    FreeCAD.ActiveDocument.recompute()
     make_rectangular_slab(
             base=wire,
-            layer='other',
-            design_type='column',
             )
+2
