fd3d091f22790696b9cde62a370579b56cd7503d
ebrahimraeyat <ebe79442114@yahoo.com>
2023-08-11 06:00:12 +0330

    Add selection option in create base foundations

--- safe/punch/py_widget/base_foundation_panel.py
+++ safe/punch/py_widget/base_foundation_panel.py
@@ -3,6 +3,10 @@ from pathlib import Path
 import FreeCAD
 import FreeCADGui as Gui
 
+from PySide2.QtWidgets import QMessageBox
+
+from safe.punch.py_widget import resource_rc
+
 punch_path = Path(__file__).parent.parent
 
 
@@ -31,23 +35,38 @@ class Form:
         height = self.form.height_spinbox.value() * 10
         soil_modulus = self.form.soil_modulus.value()
         angle = self.form.angle_spinbox.value()
+        selection = self.form.selection_checkbox.isChecked()
         doc = FreeCAD.ActiveDocument
         beams = []
-        sel = Gui.Selection.getSelection()
-        if not sel:
-            sel = doc.Objects
+        sel = []
+        if selection:
+            sel = Gui.Selection.getSelection()
+            for o in sel:
+                if (
+                    hasattr(o, "type") and
+                    o.type == 'Beam'
+                    ):
+                    beams.append(o)
+        else:
+            for o in doc.Objects:
+                if (
+                    hasattr(o, "type") and
+                    o.type == 'Beam'
+                    ):
+                    beams.append(o)
 
-        for o in sel:
-            if (
-                hasattr(o, "type") and
-                o.type == 'Beam'
-                ):
-                beams.append(o)
+        if len(beams) == 0:
+            if len(sel) > 0:
+                message = "There is No Beams in selected objects."
+            else:
+                message = "There is No Beams in Model."
+            QMessageBox.warning(None, "Beams", message)
+            return
         from safe.punch import punch_funcs
         punch_funcs.make_automatic_base_foundation(beams, width, north_dist, south_dist,
                 east_dist, west_dist, x_stirp_name, y_stirp_name, angle, height, soil_modulus)
-        Gui.Control.closeDialog()
         Gui.Selection.clearSelection()
+        Gui.Control.closeDialog()
 
     def accept(self):
         Gui.Control.closeDialog()
