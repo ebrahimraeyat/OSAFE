6e6e71ae32a1102ded0aca1e7f28e129e432aae0
ebrahimraeyat <ebe79442114@yahoo.com>
2024-10-02 09:29:04 +0330

    Export design strip to safe16 f2k file

--- osafe_py_widgets/export/export_strips_panel.py
+++ osafe_py_widgets/export/export_strips_panel.py
@@ -36,7 +36,7 @@ class Form:
 
     def set_filename(self):
         filename = Path(self.etabs.get_filename_path_with_suffix('.F2k'))
-        self.form.filename.setText(str(filename))
+        self.form.f2k_filename.setText(str(filename))
         
     def create_connections(self):
         self.form.browse.clicked.connect(self.browse)
@@ -51,28 +51,28 @@ class Form:
     def selection_changed(self):
         if self.form.safe20.isChecked():
             self.form.levels_list.setEnabled(False)
-            self.form.filename.setEnabled(False)
+            self.form.f2k_filename.setEnabled(False)
             self.form.browse.setEnabled(False)
         elif self.form.safe16.isChecked():
             self.form.levels_list.setEnabled(False)
-            self.form.filename.setEnabled(True)
+            self.form.f2k_filename.setEnabled(True)
             self.form.browse.setEnabled(True)
         elif self.form.etabs.isChecked():
             self.form.levels_list.setEnabled(True)
-            self.form.filename.setEnabled(False)
+            self.form.f2k_filename.setEnabled(False)
             self.form.browse.setEnabled(False)
 
     def browse(self):
         ext = '.f2k'
         from PySide2.QtWidgets import QFileDialog
         filters = f"{ext[1:]} (*{ext})"
-        filename, _ = QFileDialog.getSaveFileName(None, 'select file',
+        filename, _ = QFileDialog.getOpenFileName(None, 'select file',
                                                 None, filters)
         if not filename:
             return
         if not filename.lower().endswith(ext):
             filename += ext
-        self.form.filename.setText(filename)
+        self.form.f2k_filename.setText(filename)
 
     def export_strips(self):
         doc = FreeCAD.ActiveDocument
@@ -98,8 +98,36 @@ class Form:
             self.etabs.area.export_freecad_strips(doc=doc)
             self.etabs.view.refresh_view()
         elif self.form.safe16.isChecked():
-            raise NotImplementedError
+            ret = self.export_to_safe_16(doc)
+            if ret is None:
+                return
         QMessageBox.information(None, "Applied to Software", "Strips Written Successfully.")
+
+    def export_to_safe_16(self, doc):
+        filename = self.form.f2k_filename.text()
+        if not Path(filename).exists():
+            QMessageBox.warning(None, "F2k File", "Please Select a valid F2k filename.")
+            return None
+        # check input f2k file
+        from osafe_import_export.safe_read_write_f2k import get_f2k_version
+        with open(filename, 'r') as f:
+            content = f.read()
+        ver = get_f2k_version(doc=doc, content=content)
+        if ver is None:
+            QMessageBox.warning(None, "Missing Input F2k", "You don't have a Valid f2k file.")
+        elif ver < 14:
+            QMessageBox.warning(None, "Version Error", f"The version of input f2k is {ver}, please convert it to version 14 or 16.")
+            return 
+        from osafe_import_export.safe_read_write_f2k import FreecadReadwriteModel as FRW
+        from osafe_funcs import osafe_funcs
+        from freecad_funcs import add_to_clipboard
+
+        strips = osafe_funcs.get_objects_of_type("Strip", doc)
+        rw = FRW(filename, filename, doc)
+        rw.export_freecad_strips(strips)
+        rw.safe.write()
+        add_to_clipboard(filename)
+        return True
     
     def show_help(self):
         from freecad_funcs import show_help
